96cff25abdd9ab268e0f1b121e51ca96a8a85979
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-10-19 08:56:45 -0700

    Update weight.js

--- atomic-weight/weight.js
+++ atomic-weight/weight.js
@@ -5,7 +5,7 @@ const data = (function () {
       return JSON.parse(this.responseText);
     }
   };
-  xhttp.open('POST', '../data/PeriodicTableJSON.json', true);
+  xhttp.open('GET', '../data/PeriodicTableJSON.json', true);
   xhttp.send();
 })();
 
