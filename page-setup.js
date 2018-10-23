a716834f008177572b7c55a66e2b046245fdb5c2
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-10-23 08:13:58 -0700

    Create page-setup.js

---
+++ page-setup.js
@@ -0,0 +1,16 @@
+(function () {
+  document.getElementById('tab-display').style.height = (window.innerHeight - document.getElementById('topbar').offsetHeight - document.getElementById('bottombar').offsetHeight) + 'px';
+  
+  for (let i = 0; i < document.getElementsByClassName('tabbutton').length; i++) {
+    document.getElementsByClassName('tabbutton')[i].addEventListener('click', function (element) {
+      for (let j = 0; j < document.getElementsByClassName('tabbutton').length; j++) {
+        document.getElementsByClassName('tabbutton')[j].classList.remove('selected');
+      }
+      this.classList.add('selected');
+      for (let k = 0; k < document.getElementsByClassName('tab').length; k++) {
+        document.getElementsByClassName('tab')[k].style.display = 'none';
+      }
+      document.getElementById(this.innerText.toLowerCase() + 'Section').style.display = 'block';
+    });
+  }
+})();
